815079be7dd922d395d59708ec3de6b16a1503c9
Suryasgithub1 <dsurya27299@gmail.com>
2025-06-04 02:25:03 +0530

    Basic Structure of the calculator in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="min-h-screen flex flex-col items-center justify-center py-6 sm:py-12 lg:py-24">
+      <header className="mb-8 text-center">
+        <h1 className="text-4xl font-extrabold text-gray-900 dark:text-white">
+          String Calculator
+        </h1>
+      </header>
+      <main className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 w-full max-w-md">
+       
+      </main>
+    </div>
     </>
   )
 }
